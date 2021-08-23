2fa2a3f29f39f84c5be70d3a0c58056c34378c91
ablanca <ablanca@jacidi.com>
2021-08-23 09:17:52 -0400

    operators overloading

---
+++ 14_contexto_estatico/14_99_test_cttes.py
@@ -0,0 +1,7 @@
+from constantes import *
+
+# aun asi el valor puede ser modificada pero no se debe modificar
+print(CONSTANTE_1)
+
+print(Maths.EULER)
+
---
+++ 14_contexto_estatico/constantes.py
@@ -0,0 +1,8 @@
+# en python las cttes son una convencion ya que las variables siempre pueden ser modificables
+# nomenclatura de constantes estas por convencion se declaran en un archivo aparte al que podremos acceder importando dicho archivo
+
+CONSTANTE_1 = "PI 3.14"
+
+# tambien se pueden definir variables de clase
+class Maths:
+    EULER = 2.71
