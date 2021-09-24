ab777adff0fb52c5bba0cc16f9e1a47b7efc5b5f
An_White <andresblancahernandez@gmail.com>
2021-09-24 17:18:23 -0400

    add chapter 5 and 6 into automate_boring_stuff_with_python-notes

---
+++ Automate_Boring_Stuff_with_Python-notes/Chapter_6/Terminar_proyecto.py
@@ -0,0 +1,14 @@
+#pw.py - Una clave insegura
+claves={'email':'a1a2a3a4a5a6a7a8a9a0', 
+        'Blog':'VmALvQyKAxiVH5G8v01if1MLZF3sdt',
+        'Banco':'Morro123'}
+import sys, pyperclip
+if len(sys.argv)<2:
+    print('Usage: python pw.py [Cuenta]- copiar clave de cuenta')
+    sys.exit()
+
+cuenta=sys.argv[1]
+if cuenta in claves:
+    pyperclip.copy(clave[cuenta])
+else:
+    print('No hay un usuario para esa cuenta'+ cuenta)
