c85ec27f5263a6a25614ef8853913bf3898fd2eb
An_White <andresblancahernandez@gmail.com>
2022-02-01 16:45:21 -0400

    added async functions and threads pool

--- Multiprocesamiento/pool_threads.py
+++ Multiprocesamiento/pool_threads.py
@@ -4,22 +4,25 @@ import time, random
 
 logging.basicConfig(level=logging.DEBUG, format='%(threadName)s: %(message)s')
 
+
 def super_task(a):
-    init= time.time()
-    t=random.randint(2,10)
+    init = time.time()
+    t = random.randint(2, 10)
     time.sleep(t)
-    end= time.time()
-    logging.info(f'terminamos tarea nro:{a}, de duracion {end-init} init: {init} end: {end}\n ')
+    end = time.time()
+    logging.info(f'terminamos tarea nro:{a}, de duracion {end - init} init: {init} end: {end}\n ')
+
 
 if __name__ == '__main__':
     executor = TPE(max_workers=6)
-    
+
     # para usar threads las tareas se deben declarar en el mismo nivel y ejecutar una debajo de la otra para que estas se tomen todas de una vez despues el resutado se arrojara 1 a 1 conferme vayan terminando su ejecucion
-    n=0
-    while True:    
-        executor.submit(super_task,n)
-        executor.submit(super_task,n+1)
-        executor.submit(super_task,n+2)
-        n+=3
-        if n>= 30:
+    n = 0
+    while True:
+        executor.submit(super_task, n)
+        executor.submit(super_task, n + 1)
+        executor.submit(super_task, n + 2)
+
+        n += 3
+        if n >= 30:
             break
