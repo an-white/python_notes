555bd98f11bfe2527e5561243f380234d6300091
An_White <andresblancahernandez@gmail.com>
2022-02-04 16:39:13 -0400

    added mandelbrot function and plot

---
+++ mandelbrot/initial_candidates.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+np.warnings.filterwarnings("ignore")
+
+
+def complex_matrix(xmin, xmax, ymin, ymax, pixel_density):
+    re = np.linspace(xmin, xmax, int((xmax - xmin) * pixel_density))
+    im = np.linspace(ymin, ymax, int((ymax - ymin) * pixel_density))
+    return re[np.newaxis, :] + im[:, np.newaxis] * 1j
