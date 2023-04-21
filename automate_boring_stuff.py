022e8c40353ee29857f56a2a411517c93ffac085
an-white <andresblancahernandez@gmail.com>
2023-04-21 13:59:45 -0400

    feat: fixing misspelling and refactored some variables

--- automate_boring_stuff.py
+++ automate_boring_stuff.py
@@ -4,15 +4,17 @@ import time
 
 def build_json():
     # orders file must have the next structure
+
     """ orders.md
     NUMORDEN1 IDCLIENTE1
     NUMORDEN2 IDCLIENTE2
     """
-    # this structure is the easy way to build all the orden JSONs
+
+    # this structure is the easy way to build all the orders in the JSON
 
     orders_file = open('orders.md', 'r')
 
-    list_orden = orders_file.readlines()
+    list_order = orders_file.readlines()
 
     new_json = {
         "Records": [
@@ -21,20 +23,22 @@ def build_json():
     }
     json_16 = {}
 
-    for line in list_orden:
-        num_orden, id_customer = line.split(' ')
+    for line in list_order:
+        num_order, id_customer = line.split(' ')
         record = {
             "body": """{
       \"Message\" : \"{\\\"CASA\\\":0,\\\"schema\\\":\\\"dbafv\\\",\\\"data\\\":{\\\"NUMORDEN\\\":\\\"""" +
-                    num_orden +
+                    num_order +
                     """\\\",\\\"IDCLIENTE\\\":\\\"""" +
                     id_customer[0:-1] + """\\\"}}\"}"""
         }
         new_json["Records"].append(record)
 
-    new_file = open(f'sendPedido{int(time.time())}.json', 'w')
+    new_file = open(f'send_order_{int(time.time())}.json', 'w')
 
     new_file.write(json.dumps(new_json))
+
+    print(f'total orders: {len(list_order)}')
 
 
 if __name__ == '__main__':
